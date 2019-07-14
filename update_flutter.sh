b8c5bbb72ca29f16434a9157cfe4662cd1bb4251
Horus125 <ignacykuchcinski@gmail.com>
2019-07-14 12:33:29 +0200

    Revert "Update update_flutter.sh" One has to install an old version of flutter to get armadillo working, don't worry - it doesn't install globally, to go back to newest just close and open terminal again (or type reset)
    This reverts commit d03c21e05f1cc6a501e869b7d3673d7d1cb2285e.

--- update_flutter.sh
+++ update_flutter.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 rm -rf lib/flutter
-#./install_flutter.sh
+./install_flutter.sh
 #cd apps/sysui
 #source tools/environment.sh
 #../../fix_flutter.sh
